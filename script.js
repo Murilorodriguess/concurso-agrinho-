2587108b71c416b37d19f844adfa7c0c9960b9f1
Murilorodriguess <murilo.sturmer.rodrigues@escola.pr.gov.br>
2026-05-27 11:47:26 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+  alert("🌱 Pequenas atitudes no campo podem transformar o futuro do planeta!");
+});
