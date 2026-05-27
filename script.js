404df70b6fc8626b85654dfb19823c4dddecd58d
Murilorodriguess <murilo.sturmer.rodrigues@escola.pr.gov.br>
2026-05-27 12:09:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const botao = document.getElementById("btnMensagem");
+// MENU MOBILE
 
-botao.addEventListener("click", () => {
-  alert("🌱 Pequenas atitudes no campo podem transformar o futuro do planeta!");
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+// RASTREAMENTO
+
+function buscarLote(){
+
+  const lote = document.getElementById("lote").value;
+  const resultado = document.getElementById("resultado");
+
+  if(lote === ""){
+    resultado.innerHTML = "Digite um número de lote.";
+    resultado.style.color = "red";
+    return;
+  }
+
+  resultado.style.color = "#0d3b1e";
+
+  resultado.innerHTML = `
+    Lote ${lote} localizado ✅ <br><br>
+
+    Fazenda: Agro Verde Sustentável <br>
+    Região: Paraná - Brasil <br>
+    Produção: Livre de desperdício hídrico <br>
+    Emissão de carbono reduzida em 35%
+  `;
+}
+
+// FORMULÁRIO
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  alert("Mensagem enviada com sucesso!");
+
+  form.reset();
+
 });
